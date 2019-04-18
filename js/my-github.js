728ac107a6ba6bcb7aaec70290474893a9517fe3
youssef910 <youssef910>
2019-04-18 04:22:09 +0100

    first tries

--- js/my-github.js
+++ js/my-github.js
@@ -1,2 +1,16 @@
-// Write code here to communicate with Github
-
+var repoList = document.querySelector("#repos-list");
+var listItem = document.querySelector("#repos-list li ");
+listItem.style.display = "none";
+var url = "https://api.github.com/users/youssef910/repos";
+function gettingMyGitHub() {
+  fetch(url)
+    .then(request => request.json())
+    .then(repoList =>
+      repoList.forEach(function(repo) {
+        var newListItem = document.createElement("li");
+        listItem.insertAdjacentElement("afterend", newListItem);
+        newListItem.innerHTML = repo.full_name + "<br>" + repo.html_url.link();
+      })
+    );
+}
+gettingMyGitHub();
